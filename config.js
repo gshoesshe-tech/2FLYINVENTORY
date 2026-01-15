0b504da99d4ba6ae8141e499fe174163f843209b
gshoesshe-tech <gshoesshe@gmail.com>
2026-01-15 20:45:48 +0800

    Add files via upload

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // IMPORTANT: fill these in before deploying.
 // Get them from Supabase Project Settings -> API
 window.APP_CONFIG = {
-  SUPABASE_URL: "PUT_SUPABASE_URL_HERE",
-  SUPABASE_ANON_KEY: "PUT_SUPABASE_ANON_KEY_HERE"
+  SUPABASE_URL: "https://hstwuxtpdrwmfheivcyj.supabase.co",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhzdHd1eHRwZHJ3bWZoZWl2Y3lqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg0ODA0MDgsImV4cCI6MjA4NDA1NjQwOH0.EfodPWpBOxEH733f4xD8JntAB7W4OiJBrvdRvyebWUE"
 };
